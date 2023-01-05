cebe37c1b17825c275022ea2e02bf43bb507cea4
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2023-01-05 14:16:36 +0900

    temporary fix for redirect

--- src/utils/i18n/redirect.tsx
+++ src/utils/i18n/redirect.tsx
@@ -2,21 +2,23 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import languageDetector from "./languageDetector";
 
-export const useRedirect = (to?: any) => {
+export const useRedirect = (to?: string) => {
   const router = useRouter();
   to = to || router.asPath;
 
   // language detection
   useEffect(() => {
     const detectedLng = languageDetector.detect();
-    if (to.startsWith("/" + detectedLng) && router.route === "/404") {
+
+    //SHOULD CHANGE THIS  /Nekodigi/
+    if (to!.startsWith("/Nekodigi/" + detectedLng) && router.route === "/404") {
       // prevent endless loop
-      router.replace("/" + detectedLng + router.route);
+      router.replace("/Nekodigi/" + detectedLng + router.route);
       return;
     }
 
     languageDetector.cache!(detectedLng!);
-    router.replace("/" + detectedLng + to);
+    router.replace("/Nekodigi/" + detectedLng + to);
   });
 
   return <></>;
