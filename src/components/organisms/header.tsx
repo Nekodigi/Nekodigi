cc3f7c8e0c6af4fabfc96b35fd4a797e45a1dc30
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2023-02-20 22:13:25 +0900

    move to head

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -10,18 +10,6 @@ export const Header = () => {
 
   return (
     <div>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
-        rel="stylesheet"
-      />
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6199893310019553"
-        crossOrigin="anonymous"
-      ></script>
-
       <AppBar
         sx={{
           bgcolor: theme.palette.local.white,
--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -14,6 +14,21 @@ export default function Home() {
         <title>Nekodigi Portfolio</title>
         <meta name="description" content="Programmer / Designer / Student" />
         <link rel="icon" href="favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6199893310019553"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
 
       <Intro />
